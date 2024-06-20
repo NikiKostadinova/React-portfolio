b0bb37c539701897420131a328c311de335d2225
NikiKostadinova <niki_radost@yahoo.com>
2024-06-20 11:44:47 +0300

    About me section

--- src/Pages/Home/Homescreen/index.jsx
+++ src/Pages/Home/Homescreen/index.jsx
@@ -1,7 +1,10 @@
 import AboutMe from "../AboutMe";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
 import HeroSection from "../HeroSection";
 import MyPortfolio from "../MyPortfolio";
 import MySkills from "../MySkills";
+import Testimonial from "../Testimonials";
 
 export default function Home() {
     return (
@@ -10,6 +13,9 @@ export default function Home() {
             <MySkills />
             <AboutMe />
             <MyPortfolio />
+            <Testimonial />
+            <ContactMe />
+            <Footer />
 
 
         </>
